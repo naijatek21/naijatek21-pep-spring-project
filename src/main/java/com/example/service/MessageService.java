52f6d57f583d108949e1709ad2407bb1fefe3479
Chike Egbuchulam <egbuchulamchike@gmail.com>
2024-04-30 05:00:06 +0000

    starting off

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,18 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.repository.MessageRepository;
+
 public class MessageService {
+    private MessageRepository messageRepository;
+
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
+
+    
 }
+
