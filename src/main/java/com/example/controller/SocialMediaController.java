b7346679ed76cfd7cf7b962b4b4bfedc85131afe
Chike Egbuchulam <egbuchulamchike@gmail.com>
2024-05-02 06:57:29 +0000

    annotations added

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -16,28 +18,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class SocialMediaController {
 
     @PostMapping("/register")
-    public void addNewUser(){}
+    public  @ResponseBody String addNewUser(){return null;}
 
     @PostMapping("/login")
-    public void logger(){}
+    public  @ResponseBody String logger(){return null;}
 
     @PostMapping("messages")
-    public void messageGenerator(){}
+    public  @ResponseBody String messageGenerator(){return null;}
 
     @GetMapping("/messages")
-    public void messageFeed(){}
+    public  @ResponseBody String messageFeed(){return null;}
 
     @GetMapping("/messages/{message_id}")
-    public void messageRetrieve(){}
+    public @ResponseBody String messageRetrieve(@PathVariable int message_id){return null;}
 
     @DeleteMapping("/messages/{message_id}")
-    public void messageDelete(){}
+    public  @ResponseBody String messageDelete(@PathVariable int message_id){return null;}
 
     @PatchMapping("/messages/{message_id}")
-    public void messageUpdate(){}
+    public @ResponseBody String messageUpdate(@PathVariable int message_id){return null;}
 
     @GetMapping("/accounts/{account_id}")
-    public void userFeed(){}
+    public @ResponseBody String userFeed(@PathVariable int account_id){return null;}
 }
 
 // public Javalin startAPI() {
